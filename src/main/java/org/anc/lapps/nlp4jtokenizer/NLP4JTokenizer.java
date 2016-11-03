8dcd6c59e96dea3f09b3137e8e31052a32cb9b96
almahmoud <almahmoud@vassar.edu>
2016-11-02 23:52:14 -0400

    First Push

---
+++ src/main/java/org/anc/lapps/nlp4jtokenizer/NLP4JTokenizer.java
@@ -0,0 +1,183 @@
+package org.anc.lapps.nlp4jtokenizer;
+
+import edu.emory.mathcs.nlp.component.tokenizer.EnglishTokenizer;
+import edu.emory.mathcs.nlp.component.tokenizer.Tokenizer;
+import edu.emory.mathcs.nlp.component.tokenizer.token.Token;
+import org.lappsgrid.api.ProcessingService;
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.metadata.IOSpecification;
+import org.lappsgrid.metadata.ServiceMetadata;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.DataContainer;
+import org.lappsgrid.serialization.Serializer;
+import org.lappsgrid.serialization.lif.Annotation;
+import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author Alexandru Mahmoud
+ */
+
+public class NLP4JTokenizer implements ProcessingService
+{
+
+    /**
+     * The Json String required by getMetadata()
+     */
+    private String metadata;
+    private static final Logger logger = LoggerFactory.getLogger(NLP4JTokenizer.class);
+
+    public NLP4JTokenizer() { metadata = generateMetadata(); }
+
+    private String generateMetadata()
+    {
+        ServiceMetadata metadata = new ServiceMetadata();
+        metadata.setName(this.getClass().getName());
+        metadata.setDescription("The tokenizer component of EmoryNLP's NLP4J.");
+        metadata.setVersion(Version.getVersion());
+        metadata.setVendor("http://www.lappsgrid.org");
+        metadata.setLicense(Discriminators.Uri.APACHE2);
+
+        IOSpecification requires = new IOSpecification();
+        requires.addFormat(Discriminators.Uri.TEXT);
+        requires.setEncoding("UTF-8");
+
+        IOSpecification produces = new IOSpecification();
+        produces.addFormat(Discriminators.Uri.LAPPS);
+        produces.addAnnotation(Discriminators.Uri.TOKEN);
+        produces.setEncoding("UTF-8");
+
+        metadata.setRequires(requires);
+        metadata.setProduces(produces);
+
+        Data<ServiceMetadata> data = new Data<>();
+        data.setDiscriminator(Discriminators.Uri.META);
+        data.setPayload(metadata);
+        return data.asPrettyJson();
+    }
+
+    @Override
+    /**
+     * Returns a JSON string containing metadata describing the service. The
+     * JSON <em>must</em> conform to the json-schema at
+     * <a href="http://vocab.lappsgrid.org/schema/service-schema.json">http://vocab.lappsgrid.org/schema/service-schema.json</a>
+     * (processing services) or
+     * <a href="http://vocab.lappsgrid.org/schema/datasource-schema.json">http://vocab.lappsgrid.org/schema/datasource-schema.json</a>
+     * (datasources).
+     */
+    public String getMetadata() {
+        return metadata;
+    }
+
+    /**
+     * Entry point for a Lappsgrid service.
+     * <p>
+     * Each service on the Lappsgrid will accept {@code org.lappsgrid.serialization.Data} object
+     * and return a {@code Data} object with a {@code org.lappsgrid.serialization.lif.Container}
+     * payload.
+     * <p>
+     * Errors and exceptions that occur during processing should be wrapped in a {@code Data}
+     * object with the discriminator set to http://vocab.lappsgrid.org/ns/error
+     * <p>
+     * See <a href="https://lapp.github.io/org.lappsgrid.serialization/index.html?org/lappsgrid/serialization/Data.html>org.lappsgrid.serialization.Data</a><br />
+     * See <a href="https://lapp.github.io/org.lappsgrid.serialization/index.html?org/lappsgrid/serialization/lif/Container.html>org.lappsgrid.serialization.lif.Container</a><br />
+     *
+     * @param input A JSON string representing a Data object
+     * @return A JSON string containing a Data object with a Container payload.
+     */
+    @Override
+    public String execute(String input)
+    {
+        // Parse the JSON string into a Data object, and extract its discriminator.
+        Data<String> data = Serializer.parse(input, Data.class);
+        String discriminator = data.getDiscriminator();
+
+        // If the Input discriminator is ERROR, return the Data as is, since it's already a wrapped error.
+        if (Discriminators.Uri.ERROR.equals(discriminator))
+        {
+            return input;
+        }
+
+        // If the Input discriminator is not TEXT, return a wrapped Error with an appropriate message.
+        else if (!Discriminators.Uri.TEXT.equals(discriminator))
+        {
+            String errorData = generateError("Invalid discriminator.\nExpected " + Discriminators.Uri.TEXT + "\nFound " + discriminator);
+            logger.error(errorData);
+            return errorData;
+        }
+
+        // Output an error if no payload is given, since an input is required to run the program
+        if (data.getPayload() == null)
+        {
+            String errorData = generateError("No input given.");
+            logger.error(errorData);
+            return errorData;
+        }
+
+        // If a payload is given, process the input
+        else
+        {
+            Tokenizer tokenizer = new EnglishTokenizer();
+
+            String inputString = data.getPayload();
+            InputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+            Container container = new Container();
+            container.setText(inputString);
+            container.setLanguage("en");
+            View view = container.newView();
+
+            // Will track last seen position, to find index of tokens starting from that index
+            int position = 0;
+
+            for (List<Token> tokens : tokenizer.segmentize(inputStream)) {
+                for (Token token : tokens) {
+                    String tokenString = token.toString();
+
+                    int beginning = inputString.indexOf(tokenString, position);
+                    int ending = beginning + tokenString.length();
+                    position = ending;
+                    Annotation ann = view.newAnnotation("tok1", Discriminators.Uri.TOKEN, beginning, ending);
+
+                }
+            }
+
+            try
+            {
+                inputStream.close();
+            }
+
+            catch(IOException e)
+            {
+                String errorData = generateError("Error in closing input stream.");
+                logger.error(errorData);
+                return errorData;
+            }
+
+            Data returnData = new DataContainer(container);
+            return returnData.asPrettyJson();
+        }
+    }
+
+
+    /** This method takes an error message and returns it in a {@code Data}
+     * object with the discriminator set to http://vocab.lappsgrid.org/ns/error
+     *
+     * @param message A string representing the error message
+     * @return A JSON string containing a Data object with the message as a payload.
+     */
+    private String generateError(String message)
+    {
+        Data<String> data = new Data<>();
+        data.setDiscriminator(Discriminators.Uri.ERROR);
+        data.setPayload(message);
+        return data.asPrettyJson();
+    }
+
+}
---
+++ src/test/java/org/anc/lapps/nlp4jtokenizer/NLP4JTokenizerTest.java
@@ -0,0 +1,101 @@
+package org.anc.lapps.nlp4jtokenizer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.metadata.IOSpecification;
+import org.lappsgrid.metadata.ServiceMetadata;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.Serializer;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexandru Mahmoud
+ */
+public class NLP4JTokenizerTest
+{
+    private NLP4JTokenizer nlp4JTokenizer;
+
+    @Before
+    public void setup()
+    {
+        nlp4JTokenizer = new NLP4JTokenizer();
+    }
+
+    @After
+    public void cleanup()
+    {
+        nlp4JTokenizer = null;
+
+    }
+    @Test
+    public void testMetadata()
+    {
+        String jsonMetadata = nlp4JTokenizer.getMetadata();
+        assertNotNull("service.getMetadata() returned null", jsonMetadata);
+
+        Data data = Serializer.parse(jsonMetadata, Data.class);
+        assertNotNull("Unable to parse metadata json.", data);
+        assertNotSame(data.getPayload().toString(), Discriminators.Uri.ERROR, data.getDiscriminator());
+
+        ServiceMetadata metadata = new ServiceMetadata((Map) data.getPayload());
+
+        assertEquals("Vendor is not correct", "http://www.lappsgrid.org", metadata.getVendor());
+        assertEquals("Name is not correct", NLP4JTokenizer.class.getName(), metadata.getName());
+        assertEquals("Version is not correct.","1.0.0-SNAPSHOT" , metadata.getVersion());
+        assertEquals("License is not correct", Discriminators.Uri.APACHE2, metadata.getLicense());
+
+        IOSpecification produces = metadata.getProduces();
+        assertEquals("Produces encoding is not correct", "UTF-8", produces.getEncoding());
+        assertEquals("Too many annotation types produced", 1, produces.getAnnotations().size());
+        assertEquals("Too many output formats", 1, produces.getFormat().size());
+        assertEquals("LIF not produced", Discriminators.Uri.LAPPS, produces.getFormat().get(0));
+
+        IOSpecification requires = metadata.getRequires();
+        assertEquals("Requires encoding is not correct", "UTF-8", requires.getEncoding());
+        assertEquals("Requires Discriminator is not correct", Discriminators.Uri.TEXT, requires.getFormat().get(0));
+    }
+
+    @Test
+    public void testErrorInput()
+    {
+        System.out.println("NLP4JTokenizerTest.testErrorInput");
+        String message = "This is an error message";
+        Data<String> data = new Data<>(Discriminators.Uri.ERROR, message);
+        String json = nlp4JTokenizer.execute(data.asJson());
+        assertNotNull("No JSON returned from the service", json);
+
+        data = Serializer.parse(json, Data.class);
+        assertEquals("Invalid discriminator returned", Discriminators.Uri.ERROR, data.getDiscriminator());
+        assertEquals("The error message has changed.", message, data.getPayload());
+    }
+
+    @Test
+    public void testInvalidDiscriminator()
+    {
+        Data<String> data = new Data<>(Discriminators.Uri.QUERY, "");
+        String json = nlp4JTokenizer.execute(data.asJson());
+        assertNotNull("No JSON returned from the service", json);
+        data = Serializer.parse(json, Data.class);
+        assertEquals("Invalid discriminator returned: " + data.getDiscriminator(), Discriminators.Uri.ERROR, data.getDiscriminator());
+        System.out.println(data.getPayload());
+    }
+
+    @Test
+    public void testExecuteTrain()
+    {
+        System.out.println("NLP4JTokenizerTest.testExecuteTrain");
+        StringBuilder exampleSentence = new StringBuilder("This is an example of a sentence to be tokenized.\n");
+        exampleSentence.append("The tokenizer will tokenize this sentence, in such a way that the sentence will be ");
+        exampleSentence.append("tokenized at the end. :)\nI am so happy about this tokenizer :D");
+        String payloadString =  exampleSentence.toString();
+        Data<String> data = new Data<>(Discriminators.Uri.TEXT, payloadString);
+
+        String response = nlp4JTokenizer.execute(data.asJson());
+        System.out.println(response);
+    }
+
+}
